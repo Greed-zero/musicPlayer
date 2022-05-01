9fa61faae91e2ec7b0fa22e95c7f39eaa6382ce2
ght <1311325183@qq.com>
2022-05-01 21:18:58 +0800

    实现音乐的播放(歌单顺序播放)

--- src/components/play.vue
+++ src/components/play.vue
@@ -1,12 +1,37 @@
 <template>
-  <div class="player">我是播放器</div>
+  <div class="player">
+    <div>123</div>
+    <audio  
+    :src="musicsrc" 
+    autoplay
+    @ended="audioFinished">
+    </audio>
+    <div class="audiobox"></div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "play"
+  name: "play",
+  mounted(){
+  },
+  computed:{
+    musicsrc(){
+      return this.$store.state.urls[this.$store.state.musciKey].url
+    }
+  },
+  methods:{
+    audioFinished(){
+      this.$store.commit('autoMusciKey')
+    }
+  }
+
 }
 </script>
 
 <style scoped>
+.player{
+  display: flex;
+  flex-flow: row;
+}
 </style>
